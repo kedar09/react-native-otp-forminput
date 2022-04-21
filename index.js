7adf6802ad297b82e45bbbbb374ed205e49694c0
kedar09 <httpskedar@gmail.com>
2022-04-21 12:24:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OTPTextInput = () => {
+  return (
+    <View>
+      <Text>OTPTextInput</Text>
+    </View>
+  );
+};
+
+export default OTPTextInput;
